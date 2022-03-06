a655e3ef0978b838488ebbe5c0623d8f27532b78
School <kaboy@icloud.com>
2022-03-06 01:12:48 -0600

    Updated idb.js following module 18 instructions

--- public/js/idb.js
+++ public/js/idb.js
@@ -37,3 +37,48 @@ function saveRecord(record) {
   // add transaction with add method
   transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access your object store
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  // get all records from store and set to a variable
+  const getAll = transactionObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
